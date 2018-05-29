1e85fb245a29c1c2efff41987a8f1b49b0a6c35b
tcaenen <t.caenen@gmail.com>
2018-05-29 09:27:29 +0200

    Update formspree.js

--- assets/js/formspree.js
+++ assets/js/formspree.js
@@ -2,7 +2,7 @@ var $contactForm = $('#contact-form');
 $contactForm.submit(function(e) {
 	e.preventDefault();
 	$.ajax({
-		url: 'https//formspree.io/tcaenen@gmail.com',
+		url: 'https://formspree.io/tcaenen@gmail.com',
 		method: 'POST',
 		data: $(this).serialize(),
 		dataType: 'json'
